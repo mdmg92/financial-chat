e1de17bed76938bafa541633e9cba140fcf8a03d
Mathias de Mestral <mathiasdemestral@gmail.com>
2022-06-19 18:04:54 -0400

    Add Kafka support

--- src/FinancialChat.Bot/Program.cs
+++ src/FinancialChat.Bot/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCap(x =>
+{
+    x.UseInMemoryStorage();
+    x.UseKafka(k =>
+    {
+        k.Servers = builder.Configuration.GetConnectionString("Kafka");
+    });
+});
 
 var app = builder.Build();
 
