a5bd6e0edd7b78f26b9c3166a368993fa703f492
Mathias de Mestral <mathiasdemestral@gmail.com>
2022-06-19 18:12:56 -0400

    Add SearchStockInfoCommand.cs

---
+++ src/FinancialChat.Bot/Domain/Commands/SearchStockInfoCommand.cs
@@ -0,0 +1,59 @@
+﻿using DotNetCore.CAP;
+using FinancialChat.Bot.Domain.Services;
+
+namespace FinancialChat.Bot.Domain.Commands;
+
+public class SearchStockInfoCommand
+{
+    public string StockCode { get; set; }
+    public string Username { get; set; }
+}
+
+public class StockInfoReceivedEvent
+{
+    public string Username { get; set; }
+    public string Message { get; set; }
+}
+
+public interface ISearchStockInfoCommandHandler
+{
+    Task Handle(SearchStockInfoCommand command);
+}
+
+public class SearchStockInfoCommandHandler : ICapSubscribe, ISearchStockInfoCommandHandler
+{
+    private readonly StockClient _client;
+    private readonly ICapPublisher _bus;
+    private readonly ILogger<SearchStockInfoCommandHandler> _logger;
+    private static string MessageTemplate = "{0} quote is ${1} per share";
+
+    public SearchStockInfoCommandHandler(StockClient client, ICapPublisher bus,
+        ILogger<SearchStockInfoCommandHandler> logger)
+    {
+        _client = client;
+        _bus    = bus;
+        _logger = logger;
+    }
+
+    [CapSubscribe("financialchat.bot")]
+    public async Task Handle(SearchStockInfoCommand command)
+    {
+        var info = await _client.DownloadStockInfo(command.StockCode);
+
+        if (info is null)
+        {
+            return;
+        }
+
+        var message = GetFormattedMessage(info);
+
+        _bus.Publish("financialchat.api", new StockInfoReceivedEvent
+        {
+            Username = command.Username,
+            Message = message
+        });
+    }
+
+    private static string GetFormattedMessage(StockInfo info)
+        => string.Format(MessageTemplate, info.Symbol.ToUpper(), info.Close);
+}
--- src/FinancialChat.Bot/Program.cs
+++ src/FinancialChat.Bot/Program.cs
@@ -1,8 +1,11 @@
+using FinancialChat.Bot.Domain.Commands;
 using FinancialChat.Bot.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ISearchStockInfoCommandHandler, SearchStockInfoCommandHandler>();
+
 builder.Services.Configure<StockClientConfig>(builder.Configuration.GetSection(nameof(StockClientConfig)));
 
 builder.Services.AddHttpClient<StockClient>();
